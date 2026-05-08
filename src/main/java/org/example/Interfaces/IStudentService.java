71ac9121a0d72ec4998c9b4503896e2857df6a32
Zeane Denel <zeane.gon@gmail.com>
2026-05-08 22:23:25 +0800

    add a duplicate check for IStudentService - interfaces

--- src/main/java/org/example/Interfaces/IStudentService.java
+++ src/main/java/org/example/Interfaces/IStudentService.java
@@ -1,10 +1,12 @@
 package org.example.Interfaces;
 
 import org.example.Entities.Student;
+import org.example.Exceptions.DuplicateIdException;
+
 import java.util.List;
 
 public interface IStudentService {
-    void addStudent(Student student);
+    void addStudent(Student student) throws DuplicateIdException;
     void updateStudent(Student student);
     void removeStudent(String studentId);
     List<Student> getAllStudents();
