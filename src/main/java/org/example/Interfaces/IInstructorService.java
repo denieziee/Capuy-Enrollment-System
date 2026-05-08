b86fc031e0e6114b10baed20b0db5e3c3d901ec0
Zeane Denel <zeane.gon@gmail.com>
2026-05-08 22:51:54 +0800

    add a throw exception for duplicate id in IInstructorService

--- src/main/java/org/example/Interfaces/IInstructorService.java
+++ src/main/java/org/example/Interfaces/IInstructorService.java
@@ -2,10 +2,12 @@ package org.example.Interfaces;
 
 import org.example.Entities.Instructor;
 import org.example.Entities.Section;
+import org.example.Exceptions.DuplicateIdException;
+
 import java.util.List;
 
 public interface IInstructorService {
-    void addInstructor(Instructor instructor);
+    void addInstructor(Instructor instructor) throws DuplicateIdException;;
     void assignInstructorToSection(Instructor instructor, Section section);
     Instructor getInstructorDetails(String instructorId);
     List<Instructor> getAllInstructors();
