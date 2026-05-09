9da95a10e8d81d183d2cd5c337f2ae724f02dcd4
Zeane Denel <zeane.gon@gmail.com>
2026-05-09 18:33:02 +0800

    add to check if user tries to pay a negative amount or amount that exceeds their balance

---
+++ src/main/java/org/example/Exceptions/InvalidPaymentAmountException.java
@@ -0,0 +1,7 @@
+package org.example.Exceptions;
+
+public class InvalidPaymentAmountException extends RuntimeException {
+  public InvalidPaymentAmountException(String message) {
+    super(message);
+  }
+}
